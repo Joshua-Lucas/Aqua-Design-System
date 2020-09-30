a9e35839bafffa17995b2aeec7c68d852fc8169c
Joshua Lucas <joshuamlucas91@gmail.com>
2020-09-30 17:14:53 -0400

    Add an about page

---
+++ src/pages/about.js
@@ -0,0 +1,49 @@
+import React from "react"
+
+function About() {
+  return (
+    <>
+      <h1>What is Aqua</h1>
+      <h2>
+        Aqua is Jludev's design system for products and digital experience. This
+        design system is the foundation for all projects Jludev and associates
+        are developing.
+      </h2>
+
+      <h3> Welcome</h3>
+      <p>
+        Aqua is primairy used to help Jludev, but Aqua is open. The creaters of
+        Aqua hope it can be helpful for others as tey seek to create a great web
+        experience for all users
+      </p>
+      <p>
+        The goal of this design system is to imporve UI conisitancy and qaulity,
+        making the design and development process more efficent and focesed.
+      </p>
+      <p>
+        The design system is built with React and styled components. Though only
+        react now in the future, if anyone would like to contribute to brodening
+        the scope of these components to fit other JavaScript
+        Frameworks/Libraries, they are welcome too.
+      </p>
+
+      <h3>Guiding principles</h3>
+      <p>
+        <span>Aqua is Inclusive.</span> We belive a great user experience is
+        right of all people. Aqua is designed and built to be accesible to all,
+        regardless of ability or situation.
+      </p>
+      <p>
+        <span>Aqua is Rising.</span> The system is conintuelly growing, and open
+        for user import and expansion.
+      </p>
+      <p>
+        <span>Aqua puts the user first.</span> Aqua is overflowing with passion
+        for real people and making their experience on the web the best it can
+        be.
+      </p>
+    </>
+  )
+}
+
+export default About
